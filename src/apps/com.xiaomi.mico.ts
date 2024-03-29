a820a810e12e90c2dd2a08798712d160516ba640
W7256 <725618026@qq.com>
2024-03-30 00:31:05 +0800

    feat: 小爱音箱定位提示

--- src/apps/com.xiaomi.mico.ts
+++ src/apps/com.xiaomi.mico.ts
@@ -12,5 +12,18 @@ export default defineAppConfig({
         '[id="com.xiaomi.mico:id/adImage"] + [id="com.xiaomi.mico:id/ivCloseAd"]',
       snapshotUrls: 'https://i.gkd.li/import/12745621',
     },
+    {
+      key: 2,
+      name: '定位提示-开启定位服务',
+      desc: '点击取消',
+      rules: [
+        {
+          quickFind: true,
+          activityIds: 'com.xiaomi.mico.main.MainActivity',
+          matches:
+            'Button[id="com.xiaomi.mico:id/ml_alert_dialog_button2"][text="取消"]',
+        },
+      ],
+    },
   ],
 });
