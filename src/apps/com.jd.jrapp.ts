51ae217659998de10b4fdcb2dd8a21164891c248
W7256 <725618026@qq.com>
2024-04-07 16:50:16 +0800

    feat: 添加示意图

--- src/apps/com.jd.jrapp.ts
+++ src/apps/com.jd.jrapp.ts
@@ -30,6 +30,8 @@ export default defineAppConfig({
       rules:
         '@ImageButton[id="com.jd.jrapp:id/ib_close"] + RelativeLayout [text^="发现新版本"]',
       snapshotUrls: 'https://i.gkd.li/import/13628364',
+      exampleUrls:
+        'https://m.gkd.li/110102406/b4b1bf62-ab34-4b97-8dd9-5638b922e50d',
     },
     {
       key: 3,
@@ -38,6 +40,8 @@ export default defineAppConfig({
       resetMatch: 'activity',
       rules: '[text*="满意度打几分"] + ImageView',
       snapshotUrls: 'https://i.gkd.li/import/13804561',
+      exampleUrls:
+        'https://m.gkd.li/110102406/1933f731-aea0-49f1-acbf-e5941d5afa5f',
     },
   ],
 });
