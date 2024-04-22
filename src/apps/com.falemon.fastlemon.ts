3a61031602657eab10bbc670f799a9ec8e3e4fb7
W7256 <725618026@qq.com>
2024-04-22 18:10:46 +0800

    feat(快柠檬): 每日签到

---
+++ src/apps/com.falemon.fastlemon.ts
@@ -0,0 +1,42 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.falemon.fastlemon',
+  name: '快柠檬',
+  groups: [
+    {
+      key: 1,
+      matchTime: 10000,
+      name: '每日签到',
+      activityIds: 'com.faultyworld.walkthrough.MainActivity',
+      rules: [
+        {
+          key: 1,
+          matches: 'Button[desc="每日签到"]',
+          action: 'clickNode',
+          exampleUrls:
+            'https://m.gkd.li/110102406/f27b4391-d810-4b97-891b-b9fbf795e9d3',
+          snapshotUrls: 'https://i.gkd.li/i/15061120',
+        },
+        {
+          preKeys: [1],
+          key: 2,
+          action: 'clickNode',
+          matches: 'Button[desc="签到"]',
+          exampleUrls:
+            'https://m.gkd.li/110102406/dca7b680-46b0-418d-811f-3707f462b675',
+          snapshotUrls: 'https://i.gkd.li/i/15061135',
+        },
+        {
+          preKeys: [2],
+          key: 3,
+          action: 'clickNode',
+          matches: 'Button[desc="开心收下"]',
+          exampleUrls:
+            'https://m.gkd.li/110102406/da6de5a7-dcc8-4c60-9b70-3305b5025bc6',
+          snapshotUrls: 'https://i.gkd.li/i/15061136',
+        },
+      ],
+    },
+  ],
+});
